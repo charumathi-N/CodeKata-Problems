d4a706d140c53642c8270d2517f9445c52a726c7
charumathi-N <charumathinepoliyan@gmail.com>
2023-12-28 21:42:26 +0700

    first commit

---
+++ Array_Day36.js
@@ -0,0 +1,16 @@
+// Given 2 numbers N and K followed by N elements,print the number of repetition of K otherwise print '-1' if the element not found.
+// Sample Testcase :
+// INPUT
+// 6 2
+// 1 2 3 5 7 8
+// OUTPUT
+// 0
+
+// Solution:
+
+  n= [6,2];
+  m= [1,2,3,2,2,8];
+  let count=0;
+  let k= n[1];
+  const result = m.filter(number => number === k);
+  console.log(result.length-1);
